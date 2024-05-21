154ae9808f6a1fc4759ff666873c5eff3085b7ad
Shahabasalim <shahabasalim90@gmail.com>
2024-05-21 15:52:14 +0530

    added to cart

--- controller/cartcontroller.js
+++ controller/cartcontroller.js
@@ -2,6 +2,7 @@ const cartSchema = require('../model/cart')
 const User = require("../model/signup")
 const mongoose = require("mongoose")
 const product=require('../model/product')
+const { log } = require('console')
 
 
 
@@ -9,38 +10,35 @@ module.exports = {
     addToCart: async (req, res) => {
         if (req.session.email) {
             try {
-                console.log('');
                 const productId = req.params.id
                 const email = req.session.email
                 const user = await User.findOne({ email: email })
                 const userobjId = user._id
                 const productobjId = new mongoose.Types.ObjectId(productId)
                 const cart = await cartSchema.findOne({ userId: userobjId });
-                console.log(cart);
-
+                // console.log('cart:',cart);
 
                 if (!cart) {
-                    const newData = new cartSchema({
+                    const cart = new cartSchema({
                         products: { productId: productobjId, quantity: 1 },
                         userId: userobjId,
                     });
-                    await newData.save();
+                    await cart.save();
                     
                 } else {
-
-                    if (!cart.products.some(product => product.productId.equals(productobjId))) {
-                        
-
-                        // If the product doesn't exist, update the cart schema
-                        await cartSchema.updateOne(
-                            { userId: userobjId },
-                            { $push: { products: { productId: productobjId, quantity: 1 } } }
-                        );
-                    }
-                   ;
                     
-                }
-
+                    //   console.log('hi');
+                        const productIndex = cart.products.findIndex(product => product.productId.equals(productobjId));
+        
+                        if (productIndex === -1) {
+                            cart.products.push({ productId: productobjId, quantity: 1 });
+                        } else {
+                         
+                            cart.products[productIndex].quantity += 1; 
+                        }
+                        await cart.save();
+                    }
+                    res.redirect("/viewcart");
             } catch (error) {
                 console.error('Error adding to wishlist:', error);
                 res.status(500).json({ error: 'Internal Server Error' });
@@ -65,7 +63,7 @@ module.exports = {
                         // productDiscountedAmount:discountAmount
                     }
             )}
-            res.render('user/cart', { cartData , totalAmount,subtotal });
+            res.render('user/cart', { cartData ,totalAmount ,subtotal });
 
         }else {    
             res.redirect('/userLogin');
@@ -88,6 +86,7 @@ module.exports = {
             } else {
                 res.redirect('/login');
             }
+            
             
         } catch (error) {
             console.error("Error fetching cart:", error);
